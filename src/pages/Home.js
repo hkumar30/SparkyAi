4c81ae498c8940ba35a2508a847c167465a27131
hkumar30 <hkumar30@asu.edu>
2025-04-21 16:09:22 -0700

    Added advertisement video on the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 import { FaBolt, FaRocket, FaLightbulb, FaTrophy } from 'react-icons/fa';
@@ -85,51 +85,85 @@ const Tagline = styled.p`
   }
 `;
 
-const VideoPlaceholder = styled.div`
+const VideoSection = styled.div`
   width: 100%;
   max-width: 800px;
-  height: 450px;
-  background-color: ${props => props.theme.colors.lightGrey};
-  border-radius: ${props => props.theme.borderRadius.lg};
   margin-bottom: ${props => props.theme.spacing.xl};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: ${props => props.theme.boxShadow.md};
+  border-radius: ${props => props.theme.borderRadius.lg};
   overflow: hidden;
+  box-shadow: ${props => props.theme.boxShadow.md};
   position: relative;
+  background-color: ${props => props.theme.colors.lightGrey};
   
-  &::after {
-    content: 'Video Placeholder';
-    font-size: 1.25rem;
-    color: ${props => props.theme.colors.darkGrey};
+  /* Create a fixed aspect ratio container using padding trick */
+  &::before {
+    content: "";
+    display: block;
+    padding-top: 56.25%; /* 16:9 Aspect Ratio */
   }
   
   @media (max-width: 768px) {
-    height: 300px;
+    max-width: 100%;
   }
+`;
 
-  .play-button {
-    position: absolute;
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-    background-color: ${props => props.theme.colors.primary};
-    color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 2rem;
-    cursor: pointer;
-    box-shadow: ${props => props.theme.boxShadow.md};
-    transition: ${props => props.theme.transition.medium};
-    animation: ${pulse} 2s infinite ease-in-out;
-    
-    &:hover {
-      background-color: ${props => props.theme.colors.lightPrimary};
-      transform: scale(1.1);
-    }
+const PlayButtonWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 5;
+`;
+
+const PlayButton = styled.div`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background-color: ${props => props.theme.colors.primary};
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  box-shadow: ${props => props.theme.boxShadow.md};
+  transition: ${props => props.theme.transition.medium};
+  animation: ${pulse} 2s infinite ease-in-out;
+  
+  svg {
+    margin: 0; /* Remove any potential margin from the SVG */
+    padding: 0; /* Remove any potential padding from the SVG */
+    font-size: 1.5rem; /* Explicitly set the size of the icon */
   }
+  
+  &:hover {
+    background-color: ${props => props.theme.colors.lightPrimary};
+    transform: scale(1.1);
+  }
+`;
+
+const ResponsiveIframe = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+`;
+
+const ThumbnailPlaceholder = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${props => props.theme.colors.lightGrey};
+  background-image: url("https://img.youtube.com/vi/0uKC5reBiqI/maxresdefault.jpg");
+  background-size: cover;
+  background-position: center;
 `;
 
 const ModesSection = styled.section`
@@ -233,6 +267,7 @@ const FeaturesList = styled.ul`
 const Home = () => {
   const navigate = useNavigate();
   const { currentUser } = useAuth();
+  const [videoPlaying, setVideoPlaying] = useState(false);
   
   const handleModeSelect = (mode) => {
     const path = `/${mode.toLowerCase().replace(' ', '-')}`;
@@ -246,6 +281,10 @@ const Home = () => {
     }
   };
   
+  const handlePlayClick = () => {
+    setVideoPlaying(true);
+  };
+  
   return (
     <HomeContainer>
       <HeroSection>
@@ -255,11 +294,26 @@ const Home = () => {
         </LogoContainer>
         <Tagline>Make Writing Fun Again</Tagline>
         
-        <VideoPlaceholder>
-          <div className="play-button">
-            <FaBolt />
-          </div>
-        </VideoPlaceholder>
+        <VideoSection>
+          {!videoPlaying && (
+            <>
+              <ThumbnailPlaceholder />
+              <PlayButtonWrapper>
+                <PlayButton onClick={handlePlayClick}>
+                  <FaBolt />
+                </PlayButton>
+              </PlayButtonWrapper>
+            </>
+          )}
+          {videoPlaying && (
+            <ResponsiveIframe
+              src="https://www.youtube.com/embed/0uKC5reBiqI?autoplay=1"
+              title="Sparky AI Demo"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          )}
+        </VideoSection>
         
         <ModesContainer>
           <ModeCard>
